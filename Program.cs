a36a891febef2e4d04ef9c9a11ba3b05e8a737f9
Scott Zatzkis <szatzkis@gmail.com>
2017-06-24 20:15:21 -0600

    Fizz Buzz
    loop for console app

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz(50, 80);
+            Console.ReadKey();
+        }
+
+        static void FizzBuzz(int number, int number2)
+        {
+            
+            for(int i = number; number <= number2; number++)
+            {
+                if (number % 5 == 0 && number % 3 == 0) 
+                {
+                    Console.Write("Fizz");
+                    Console.ReadKey();
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.Write("Buzz");
+                    Console.ReadKey();
+                }
+                else if(number % 5 == 0)                {
+                    Console.Write("FizzBuzz");
+                    Console.ReadKey(); 
+                }
+
+
+            }
+         }
+    }
+
+}

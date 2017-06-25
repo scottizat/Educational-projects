b5a11d4a97e6a3ae028e182285d08fc02280ba9f
Scott Zatzkis <szatzkis@gmail.com>
2017-06-24 20:36:36 -0600

    Revert "Revert "array prax""
    This reverts commit 5e62e000843a6e5362e4986a32e67fc99c7d0750.

---
+++ methodPrax.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Man
+{
+    class Program
+    {
+        /* static void Main(string[] args)
+         {
+             HelloWorld();
+             Console.ReadLine();
+
+         }
+
+
+         private static void HelloWorld()
+         {
+             Console.WriteLine("Hello World!");
+         }*/
+
+
+
+
+                //Console.Write(Results: ); move this to the write area below
+           // string reversedFirstName = ReverseString(firstName); We can get rid of all this with the code below
+           // string reversedLastName = ReverseString(lastName);
+           // string reversedCity = ReverseString(city);
+
+                DisplayResult(
+                    ReverseString(firstName),
+                    ReverseString(lastName),
+                    ReverseString(city));
+
+    }
+
+    /*  private static void ReverseString(string message)
+    {
+
+      char[] messageArray = message.ToCharArray();
+      Array.Reverse(messageArray);
+      foreach(char item in messageArray)
+      {
+          Console.Write(item);
+      }
+
+      Console.Write(" "); 
+    }
+     */
+     {
+          /*   Console.Write("Results: ");
+                string reversedFirstName = ReverseString(firstName);
+                string reversedLastName = ReverseString(lastName);
+                string reversedCity = ReverseString(city);*/
+
+
+           
+
+
+    private static string ReverseString(string message)  //notice we are asking to return a string- not void.
+    {
+
+        char[] messageArray = message.ToCharArray();
+        Array.Reverse(messageArray);
+        return string.Concat(messageArray);
+
+    }
+   
+
+
+      {
+             private static void DisplayResult(
+                 string reversedFirstName,
+                 string reversedLastName,
+                 string reversedCity);
+
+            Console.Write(Results: ");  
+
+            Console.Write(string.format("{0}, {1}, {2}",
+                reversedFirstName,
+                reversedLastName,
+                reversedCity));
+
+            Console.ReadLine();
+
+      {
+
+                Console.Write
+
+
+      }
+   
+
+
+}
+
+
+   
+
+
+
+
+
+}
